8aea34ec9d2723110dc7ff01e7c73a81b2b9b943
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-10-14 15:49:48 +0200

    refactoring, fixes working create, find rest api

--- test/lib/functional/instances.restapi.test.js
+++ test/lib/functional/instances.restapi.test.js
@@ -5,40 +5,35 @@ const o = require('../common');
 const url = 'http://localhost:3000/instances';
 
 describe('instances restapi', () => {
+  const doc = {
+    hostname: 'foo.com',
+    ip: '12.34.56.78',
+  };
   it('create instance', (done) => {
     o.superagent.post(url)
-      .send({hostname: 'foo.com'})
+      .send(doc)
       .set('Accept', 'application/json')
       .end((err, res) => {
         if (err) {
           done(err);
         } else {
-          o.assert.strictEqual(res.body.hostname, 'foo.com');
+          o.assert(res.body.id);
+          doc.id = res.body.id;
+          o.assert.strictEqual(res.body.hostname, doc.hostname);
+          o.assert.strictEqual(res.body.ip, doc.ip);
           done();
         }
       });
   });
-  it.skip('get instance', (done) => {
-    o.superagent.get(url + '?hostname=foo.com')
-      .send({hostname: 'foo.com'})
-      .set('Accept', 'application/json')
+  it('get instance', (done) => {
+    o.superagent.get(url + '?id=' + doc.id)
       .end((err, res) => {
         if (err) {
           done(err);
         } else {
-          o.assert.strictEqual(res.body.hostname, 'foo.com');
-          done();
-        }
-      });
-  });
-  it.skip('delete instance', (done) => {
-    o.superagent.delete(url)
-      .send({hostname: 'my.hostname'})
-      .set('Accept', 'application/json')
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        } else {
+          o.assert.strictEqual(res.body[0].id, doc.id);
+          o.assert.strictEqual(res.body[0].hostname, doc.hostname);
+          o.assert.strictEqual(res.body[0].ip, doc.ip);
           done();
         }
       });
