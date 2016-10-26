e35e2a96ee3cf0d38155e84d95e30db05356abe7
solaris69 <vmanin@outlook.com>
2016-10-26 14:19:57 +0700

    [WIP] Update unit test, add MongoDB fixture for testing restapi functionals.

--- test/lib/functional/instances.restapi.test.js
+++ test/lib/functional/instances.restapi.test.js
@@ -4,38 +4,55 @@ const o = require('../common');
 
 const url = 'http://localhost:3000/instances';
 
+const db_name = 'oss';
+let dbHost = process.env.MONGO_HOST || '127.0.0.1';
+let fixtures;
+
 describe('instances restapi', () => {
   const doc = {
     hostname: 'foo.com',
     ip: '12.34.56.78',
   };
-  it('create instance', (done) => {
-    o.superagent.post(url)
-      .send(doc)
-      .set('Accept', 'application/json')
-      .end((err, res) => {
+
+context('Bricks: Start Execution', () => {
+    before((done) => {
+        fixtures = require('pow-mongodb-fixtures').connect(db_name, {
+            host: dbHost,
+            port: 27017
+        });
+        fixtures.clearAllAndLoad(__dirname + '/fixtures/instances.restapi.test.js', () => {
+            done();
+        });
+    });
+    it('create instance', (done) => {
+        o.superagent.post(url)
+        .send(doc)
+        .set('Accept', 'application/json')
+        .end((err, res) => {
         if (err) {
-          done(err);
+            done(err);
         } else {
-          o.assert(res.body.id);
-          doc.id = res.body.id;
-          o.assert.strictEqual(res.body.hostname, doc.hostname);
-          o.assert.strictEqual(res.body.ip, doc.ip);
-          done();
-        }
-      });
-  });
-  it('get instance', (done) => {
-    o.superagent.get(url + '?id=' + doc.id)
-      .end((err, res) => {
+            o.assert(res.body.id);
+    doc.id = res.body.id;
+    o.assert.strictEqual(res.body.hostname, doc.hostname);
+    o.assert.strictEqual(res.body.ip, doc.ip);
+    done();
+    }
+    });
+    });
+    it('get instance', (done) => {
+        o.superagent.get(url + '?id=' + doc.id)
+        .end((err, res) => {
         if (err) {
-          done(err);
+            done(err);
         } else {
-          o.assert.strictEqual(res.body[0].id, doc.id);
-          o.assert.strictEqual(res.body[0].hostname, doc.hostname);
-          o.assert.strictEqual(res.body[0].ip, doc.ip);
-          done();
-        }
-      });
-  });
+            o.assert.strictEqual(res.body[0].id, doc.id);
+    o.assert.strictEqual(res.body[0].hostname, doc.hostname);
+    o.assert.strictEqual(res.body[0].ip, doc.ip);
+    done();
+    }
+    });
+    });
+    });
 });
+
