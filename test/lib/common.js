0b376b128d902be1f0e2af1af34c459dbaf85b4b
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-11-09 14:44:21 +0100

    start app before rest tests

--- test/lib/common.js
+++ test/lib/common.js
@@ -5,4 +5,8 @@ const chai = require('chai');
 module.exports = {
   assert: chai.assert,
   superagent: require('superagent'),
+  forever: require('forever-monitor'),
+  exec: require('child_process').exec,
+  path: require('path'),
+  os: require('os'),
 };
