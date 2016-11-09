ae7c1c6267cd9f73b3c0a15ab809262b2fba8ebc
Adnane SAGHIR <adnane.saghir@thomsonreuters.com>
2016-11-09 14:53:34 +0100

    refactoring rest tests to use single bootstrap

--- test/lib/functional/instances.restapi.test.js
+++ test/lib/functional/instances.restapi.test.js
@@ -6,32 +6,6 @@ const url = 'http://localhost:3000/instances';
 
 describe('instances restapi', function() {
 
-  let child;
-
-  before(function(done) {
-    this.timeout(5000);
-    const file = o.path.resolve(__dirname, '..', '..', '..', 'lib', 'index.js');
-    child = new(o.forever.Monitor)(file, {
-      max: 0,
-      silent: true,
-      logFile: o.path.resolve(o.os.tmpDir(), 'cta-app-instancedataservice-main.log'),
-      outFile: o.path.resolve(o.os.tmpDir(), 'cta-app-instancedataservice-out.log'),
-      errFile: o.path.resolve(o.os.tmpDir(), 'cta-app-instancedataservice-err.log'),
-    });
-    child.on('start', function() {
-      setTimeout(done, 2000);
-    });
-    child.start();
-  });
-
-  after(function(done) {
-    this.timeout(3000);
-    child.on('stop', function() {
-      done();
-    });
-    child.stop();
-  });
-
   const doc = {
     hostname: 'foo.com',
     ip: '12.34.56.78',
